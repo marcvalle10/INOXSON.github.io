31e4897d19fc7ce7a5398470f3477f363ed5cf0b
marcvalle10 <134673564+marcvalle10@users.noreply.github.com>
2024-10-17 00:45:21 -0700

    Delete Nueva carpeta directory

--- Nueva carpeta/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.php
-session_start();
-session_destroy(); // Destruye la sesión actual
-header("Location: ../login.php"); // Redirige al login después de cerrar la sesión
-exit();
-?>
-    
--- Nueva carpeta/utilities-animation.php
+++
@@ -1,3 +0,0 @@
-<?php require_once "vistas/parte_superior.php"; ?>
-Tablas 
-<?php require_once "vistas/parte_inferior.php"; ?> 
